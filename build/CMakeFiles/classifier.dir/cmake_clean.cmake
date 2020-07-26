b86029f91af28807806c8914ea13100a6e239522
ProfHariSeldon <nano2ba@gmail.com>
2020-07-26 18:23:48 -0400

    Build test 12 deprecated  elements

---
+++ build/CMakeFiles/classifier.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/classifier.dir/dictionary/PredictImage.cpp.o"
+  "CMakeFiles/classifier.dir/dictionary/TrainSVM.cpp.o"
+  "CMakeFiles/classifier.dir/dictionary/main.cpp.o"
+  "classifier"
+  "classifier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/classifier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
